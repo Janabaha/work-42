7ff578286eef3d9c3146a76037a88b54c90a07f9
Janabaha <abukhaitjana@gmail.com>
2026-04-07 23:17:51 +0300

    Add files via upload
    added

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,28 @@
+unsigned int	strlength(char *src)
+{
+	unsigned int	length;
+
+	length = 0;
+	while (src[length])
+	{
+		length++;
+	}
+	return (length);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (i < size - 1 && src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (strlength(src));
+}
